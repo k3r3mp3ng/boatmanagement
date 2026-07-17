1589bfeb87dd4c56fd278a03585ca7a2f7e6d3e2
k3r3mp3ng <swadikapkontolodon@gmail.com>
2026-07-17 10:24:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ===========================================================
 //  KONFIGURASI & GLOBAL STATE
 // ===========================================================
-const API_BASE = 'https://script.google.com/macros/s/AKfycbwTCf6r6Bo0kt-6WJjTd4cizimKoFPeSNZJQBQnxHBpLmYiqwFSm69K-_LMWJaYSxU5sw/exec'; // Ganti dengan URL Web App
+const API_BASE = 'https://script.google.com/macros/s/AKfycbyjXNLEDO5u13McGuQ9FJqIZxL_ef1Ncv4K_GZ1hXscKTTtHALOVepoJEUEX4Ywg_PaHQ/exec'; // Ganti dengan URL Web App
 const USE_DEMO = !API_BASE || API_BASE.includes('YOUR_GOOGLE');
 
 let currentUser = null;
